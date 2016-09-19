6397092445ce7322de0410a792a26d7b85faa177
dhineshwaranr <dhinesh.kr13@gmail.com>
2016-09-20 05:13:28 +0530

    first commit

---
+++ TAGIG/src/main/java/com/ig/tag/TagigApplication.java
@@ -0,0 +1,12 @@
+package com.ig.tag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagigApplication.class, args);
+	}
+}
